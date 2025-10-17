444b4a96ad1ff0c84f5f60f32b2371b316af8aa6
ak1bhasan <akibhasan011@gmail.com>
2025-10-18 01:46:45 +0600

    first run

---
+++ First/First.asm
@@ -0,0 +1,18 @@
+.model small
+.stack 100h
+
+.data
+;all variables are defined there
+
+.code
+
+MOV AX, 0FFFFH
+MOV BX, 0FFFFH
+
+add AX, BX
+           
+
+;mov al, 80h
+;mov bl, 80h
+
+;add al, bl
